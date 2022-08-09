9337cb1dec87d57ed1e758c706644a13993a45d9
Sameer <sameer.writes.70@gmail.com>
2022-08-09 13:46:22 +0530

    dom-project

--- dom-2-counter/script.js
+++ dom-2-counter/script.js
@@ -1,1 +1,21 @@
 console.log("Working");
+
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        const style = e.currentTarget.classList;
+        if (style.contains("decrease")) {
+            count--;
+        } else if (style.contains("increase")) {
+            count++;
+        } else {
+            alert("You are Reseting the value")
+            count = 0;
+        }
+        value.textContent = count;
+    });
+});
--- readme.md
+++ readme.md
@@ -2,4 +2,9 @@
 Guys If you will click on the Image then you reach at the video Of particular project
 </h1>
 
+<h4> Project 1-: Color Flipper</h4>
+
 [![Image alt text](screenshot.png)](https://www.linkedin.com/posts/sameer-varshney-63a727203_javascript-project-aircampus-activity-6962432008790634496-NAVe?utm_source=linkedin_share&utm_medium=member_desktop_web)
+
+<h4> Project 1-: Counter Project</h4>
+[![Image alt text](screenshot2.png)
